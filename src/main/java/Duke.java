85f288155a0490d5616ff4381804e0045e3b05d6
karansarat <karansarat@ymail.com>
2019-09-02 21:57:51 +0800

    Initial Test Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    /** main. */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        Scanner input = new Scanner(System.in); // new input object
+        String[] tasks = new String[100];
+        int taskNum = 0;
+        while (true) {
+            String userIn = input.nextLine();
+            if (userIn.equals("bye")) {
+                break;
+            } else if (userIn.equals("list")) {
+                for (int i = 0; i < taskNum; i++) {
+                    System.out.println("    " + Integer.toString(i + 1) + ". " + tasks[i]);
+                }
+                continue;
+            }
+            tasks[taskNum] = userIn;
+            taskNum++;
+
+            System.out.println("    added: " + userIn);
+        }
+        System.out.println("    Bye. Hope to see you again soon!");
+
+
     }
 }
