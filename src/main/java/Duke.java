db395868a813c2e305ff2e8bcb6ef5f0fc82158b
karansarat <karansarat@ymail.com>
2019-09-03 20:00:43 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,38 @@
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Duke {
     public static final String dashLine = "    ____________________________________________________________\n";
+
+    public static void store(Hashtable lookup, int count, String userIn) {
+        lookup.put(count, userIn);
+        System.out.println(dashLine + "\tadded: " + userIn + "\n" + dashLine);
+    }
+
+    public static void list(Hashtable lookup, int count) {
+        System.out.println(dashLine);
+        for (int i = 1; i <= count; i++) {
+            System.out.println("\t" + i + ". " + lookup.get(i));
+        }
+        System.out.println(dashLine);
+    }
     /** main. */
     public static void main(String[] args) {
-        System.out.println(dashLine + "    Hello! I'm Duke\n    What can I do for you?\n" + dashLine);
+        System.out.println(dashLine + "\tHello! I'm Duke\n\tWhat can I do for you?\n" + dashLine);
         Scanner input = new Scanner(System.in); // new input object
+        Hashtable<Integer, String> lookup = new Hashtable<Integer, String>();
+        int count = 0;
         while (true) {
             String userIn = input.nextLine();
             if (userIn.equals("bye")) {
                 break;
             }
-            System.out.println(dashLine + "    " + userIn + "\n" + dashLine);
+            store(lookup, ++count, userIn);
+
+            if (userIn.equals("list")) {
+                list(lookup, count);
+            }
         }
-        System.out.println(dashLine + "    Bye. Hope to see you again soon!\n" + dashLine);
+        System.out.println(dashLine + "\tBye. Hope to see you again soon!\n" + dashLine);
     }
 }
