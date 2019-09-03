1a639f584918346323a246ed6979209c467b5b7b
karansarat <karansarat@ymail.com>
2019-09-03 16:46:58 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,19 @@
 import java.util.Scanner;
 
+
 public class Duke {
+    public static final String dashLine = "    ____________________________________________________________\n";
     /** main. */
     public static void main(String[] args) {
-        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.println(dashLine + "    Hello! I'm Duke\n    What can I do for you?\n" + dashLine);
         Scanner input = new Scanner(System.in); // new input object
-        String[] tasks = new String[100];
-        int taskNum = 0;
         while (true) {
             String userIn = input.nextLine();
             if (userIn.equals("bye")) {
                 break;
-            } else if (userIn.equals("list")) {
-                for (int i = 0; i < taskNum; i++) {
-                    System.out.println("    " + Integer.toString(i + 1) + ". " + tasks[i]);
-                }
-                continue;
             }
-            tasks[taskNum] = userIn;
-            taskNum++;
-
-            System.out.println("    added: " + userIn);
+            System.out.println(dashLine + "    " + userIn + "\n" + dashLine);
         }
-        System.out.println("    Bye. Hope to see you again soon!");
-
-
+        System.out.println(dashLine + "    Bye. Hope to see you again soon!\n" + dashLine);
     }
 }
